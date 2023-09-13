7193d27490f27a0a606b3258f1c932bcdcf9d34e
Ch Farrukh Rafique <56925356+chfrukh@users.noreply.github.com>
2023-09-13 19:37:27 +0100

    Create while.cpp

---
+++ Loops/while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1; // Initialize the loop control variable
+
+    // Use a while loop to print numbers from 1 to 10
+    while (i <= 10) {
+        cout << i << " ";
+        i++; // Increment the loop control variable
+    }
+
+    cout << endl; // Print a newline to separate the output
+
+    return 0;
+}
