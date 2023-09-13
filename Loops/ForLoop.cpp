bc51d316c584a42d3a1d2fe9c47768491094542c
Ch Farrukh Rafique <56925356+chfrukh@users.noreply.github.com>
2023-09-13 19:35:28 +0100

    Create ForLoop.cpp

---
+++ Loops/ForLoop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Use a for loop to print numbers from 1 to 10
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
+
+    cout << endl; // Print a newline to separate the output
+
+    return 0;
+}
